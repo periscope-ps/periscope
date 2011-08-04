c5d7b3e160aa8df26585be95e8bbe92f30763b84
Priscilla <priumoraes@gmail.com>
2011-08-04 11:35:58 -0400

    kNN remotion and addition of Feature subset selection method.

--- lib/python/periscope/analysis/analysis.py
+++ lib/python/periscope/analysis/analysis.py
@@ -3,9 +3,19 @@ import orange
 import utils
 import orngTest, orngStat, orngTree
 
+""" This is a temporary file, it is being used to
+test and print the current implemented classifiers.
+The way learners and classifiers will be called will be replaced
+by dataIO.py file
+"""
+
 inputfile = os.getcwd() + "\\data\\jobs.tab"
 data = orange.ExampleTable(inputfile)
 size = len(data)
+
+# For feature subset selection
+#fss = fss.Fss()
+#fss.checkAttRelevance(data, 0.0)
 
 learners = ["bayes", "tree"]
 
@@ -13,6 +23,7 @@ ut = utils.Utils()
 
 # Get the percentage of missing values per attribute within a data set
 ut.missingvalues(data)
+
 
 for i in learners:
     print "ANALYZING NEW METHOD..."
@@ -25,30 +36,6 @@ for i in learners:
     # Proabilities assigned to misclassified instances that are above or below given thresholds. If last attribute is true, prints details.
     ut.prob_misclassification(data, size, i, 0.05, 0.95, False)
     
-
-
-#For some unkown reason, this code is not handling OO calls. It breaks usually at the crossvalidation method call (that is a built-in method).
-# It is working here though.
-def evaluation(data, folds):
-    """
-    Evaluates Bayes and DT and prints results for 4 different metrics: Correspondence analysis,
-    Information score, Brier score and Area under the ROC curve.
-    """
-    bayes = orange.BayesLearner()
-    tree = orngTree.TreeLearner(mForPruning=2)
-    bayes.name = "bayes"
-    tree.name = "tree"
-    learners = [bayes, tree]
-    print "Statistical measures per learner (using %d-fold cross-validation):" % (folds)
-    results = orngTest.crossValidation(learners, data, folds)
-    print "Learner   CA      IS      Brier     AUC"
-    for i in range(len(learners)):
-        print "%-8s %5.3f  %5.3f  %5.3f  %5.3f" % (learners[i].name, orngStat.CA(results)[i], orngStat.IS(results)[i], orngStat.BrierScore(results)[i], orngStat.AUC(results)[i])
-    print "-------"
-    print
-
-# Compares bayes and tree providing statistical measures for each
-evaluation(data, 10)
 
 # Evaluates and suggests a method according to the data
 ut.choosingmethod(data)
