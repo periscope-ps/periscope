e6cf5397ffa805fb305c9c9344e463514f0367f7
Ahmed El-Hassany <ahassany@indiana.edu>
2011-10-15 10:40:59 -0400

    Add ports registration for ANI test

---
+++ peri-django/periscope/examples/anitestbed_register_pulling.py
@@ -0,0 +1,35 @@
+
+from periscope.topology.models import *
+from periscope.measurements.lib import *
+
+ports = [
+    #'urn:ogf:network:domain=testbed.es.net:node=newy-diskpt-1:port=eth1', 
+    'urn:ogf:network:domain=testbed.es.net:node=newy-diskpt-1:port=eth4',
+    'urn:ogf:network:domain=testbed.es.net:node=newy-diskpt-1:port=eth5',
+    #'urn:ogf:network:domain=testbed.es.net:node=newy-diskpt-1:port=eth8',
+    #'urn:ogf:network:domain=testbed.es.net:node=newy-diskpt-1:port=eth9',
+    
+    #'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-2:port=eth4',
+    'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-2:port=eth7',
+    #'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-2:port=eth8',
+    #'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-2:port=eth9',
+    
+    #'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-1:port=eth4',
+    'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-1:port=eth5',
+    #'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-1:port=eth8',
+]
+
+service = 'http://172.31.255.210:9990/perfSONAR_PS/services/SNMPMA'
+
+port_events = [
+    'http://ggf.org/ns/nmwg/tools/ganglia/network/utilization/bytes/2.0',
+    'http://ggf.org/ns/nmwg/tools/ganglia/network/utilization/bytes/received/2.0'
+]
+for urn in ports:
+    try:
+        port = Port.objects.get(unis_id=urn)
+    except:
+        print "Cannot find port:", urn, "\n"
+    for event in port_events:
+        r = register_pull_network_object(port, event, service)
+    
