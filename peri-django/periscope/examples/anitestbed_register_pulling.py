9372f13f32172d7b8ac8c0c572096ea5ea0ef088
root <root@monitor.damslab.org>
2011-10-16 16:17:33 -0400

    get host stats from ganglia topo updates

--- peri-django/periscope/examples/anitestbed_register_pulling.py
+++ peri-django/periscope/examples/anitestbed_register_pulling.py
@@ -19,12 +19,26 @@ ports = [
     #'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-1:port=eth8',
 ]
 
+nodes = [
+    'urn:ogf:network:domain=testbed.es.net:node=newy-diskpt-1',
+    'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-1',
+    'urn:ogf:network:domain=testbed.es.net:node=bnl-diskpt-2',
+]
+
 service = 'http://172.31.255.210:9990/perfSONAR_PS/services/SNMPMA'
 
 port_events = [
     'http://ggf.org/ns/nmwg/tools/ganglia/network/utilization/bytes/2.0',
     'http://ggf.org/ns/nmwg/tools/ganglia/network/utilization/bytes/received/2.0'
 ]
+
+node_events = [
+    'http://ggf.org/ns/nmwg/tools/ganglia/network/utilization/bytes/received/2.0',
+    'http://ggf.org/ns/nmwg/tools/ganglia/network/utilization/bytes/sent/2.0',
+    'http://ggf.org/ns/nmwg/tools/ganglia/cpu/utilization/system/2.0',
+    'http://ggf.org/ns/nmwg/tools/ganglia/memory/main/free/2.0',
+]
+
 for urn in ports:
     try:
         port = Port.objects.get(unis_id=urn)
@@ -33,3 +47,13 @@ for urn in ports:
     for event in port_events:
         r = register_pull_network_object(port, event, service)
     
+
+
+for urn in nodes:
+    try:
+        node = Node.objects.get(unis_id=urn)
+    except:
+        print "Cannot find node:", urn, "\n"
+    for event in node_events:
+        r = register_pull_network_object(node, event, service)
+
