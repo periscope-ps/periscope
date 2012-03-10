a2379c6594fb9e95c0f30df27f3699110a6a0d2d
Dan Gunter <dkgunter@lbl.gov>
2012-03-09 21:40:04 -0800

    fixed xsp/bson send

--- blipp/blipp/output.py
+++ blipp/blipp/output.py
@@ -4,11 +4,13 @@ Output classes and functions.
 Author: Dan Gunter <dkgunter@lbl.gov>
 """
 import logging
+import socket
 import sys
 
 import simplejson as json
 
-from blipp.util import bson_decode, log_formatter
+from blipp import util, xsplib
+from blipp.util import bson_encode, log_formatter
 
 class SinkException(Exception):
     pass
@@ -24,21 +26,30 @@ class Sink:
         pass
     
 class XSP_sink(Sink):
+    XSP_TYPE = 0x20 # xsp msg type
     def __init__(self, host, port):
+        Sink.__init__(self)
+        self._connect(host, port)
+
+    def _connect(self, host, port):
+        status = 0
         self.log.debug("xsp_connect.start")
         try:
             self.xsp_sess = xsplib.XSPSession()
             self.xsp_sess.connect(host, port)
         except Exception, err:
-            g_log.critical("xsp_connect.error, msg=%s host=%s, port=%d, details=%s",
-                           "failed connection to xsp",
-                           host, port, util.traceback())
+            self.log.critical("xsp_connect.error, msg=%s host=%s, port=%d, details=%s",
+                              "failed connection to xsp",
+                              host, port, util.traceback())
             raise SinkException("XSP connect error")
         self.log.debug("xsp_connect.end, status=%d", status)
+        return status
         
     def add(self, data):
         bsondata = bson_encode(data)
-        self.xsp_sess.send_msg(data, len(data), XSP_TYPE)
+        self.log.debug("xsp_send.start len=%d", len(bsondata))
+        self.xsp_sess.send_msg(bsondata, len(bsondata), self.XSP_TYPE)
+        self.log.debug("xsp_send.end len=%d status=0", len(bsondata))
 
     def close(self):
         try:
