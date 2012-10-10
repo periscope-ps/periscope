8e4e9ac50fa3c1c764e03a77db379dff7f370919
Ahmed El-Hassany <ahassany@indiana.edu>
2012-10-10 16:03:00 -0400

    [split] Squashed commit of the following:
    commit 32fc690680db732ed274ce9d57ffdbe74aa8d2fa
    Author: mjaffee <mjaffee@indiana.edu>
    Date:   Tue Oct 9 21:56:11 2012 -0400
    
        convert ubuntu install tarball script to fedora - untested
    
    commit ee8b8fe3bcd8d73d241bb3d2752e228ef2adf623
    Author: mjaffee <mjaffee@indiana.edu>
    Date:   Tue Oct 9 14:22:11 2012 -0400
    
        adding tarball install script

---
+++ newblipp/ubuntu-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Self-extracting bash script that installs blipp
+#
+# create self extracting tarball like this (from parent dir): 
+# 'tar zc blipp | cat blipp/ubuntu-install.sh - > blipp.sh'
+# Supports: Debian based distributions
+# Depends: python 2.6
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+SKIP=`awk '/^__TARFILE_FOLLOWS__/ { print NR + 1; exit 0; }' $0`
+THIS=`pwd`/$0
+tail -n +$SKIP $THIS | tar -xz
+
+# Installation steps for LAMP Toolkit
+DIR=newblipp
+ETC=/usr/local/etc
+
+apt-get -y install libnl-dev
+cd ${DIR}
+python ./setup.py install 
+
+exit 0
+
+__TARFILE_FOLLOWS__
