2d792e0028c38ea56dc0fdea8da0d1cb740161d5
Captain Underpants <mkeele@indiana.edu>
2014-05-14 01:12:05 -0400

    add favicon

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ mongoose.connect(database.url);
 app.configure(function() {
   app.set('port', process.env.PORT || 3000);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
