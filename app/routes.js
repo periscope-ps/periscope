495eacc047ec98c8ac10b1055caaf4d3a8bba278
Captain Underpants <mkeele@indiana.edu>
2014-05-14 20:24:31 -0400

    add route for slice info

--- app/routes.js
+++ app/routes.js
@@ -4,10 +4,47 @@
  * routes.js
  */
 
-var Node = require('./models/node');
-var Service = require('./models/service');
+var Node = require('./models/node')
+  , Service = require('./models/service')
+  , fs = require('fs')
+  , path = require('path');
 
-module.exports = function(app) {
+
+module.exports = function(app) {  
+
+  app.get('/slice', function(req, res) {     
+
+    var store = [];
+    var filePath = '/usr/local/etc/node.info';
+
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data) {
+      if (err) {
+        console.log(err);
+        res.send(err);
+      } else {
+        // console.log('received data: ' + data);
+        
+        var fileData = data.toString().split('\n');
+        var split, project, slice, gn;
+
+        for(line in fileData) {
+          split = fileData[line].split('=');
+          
+          if (split[0] === 'gn_address')
+            gn = split[1];
+
+          if (split[0] === 'node_id') {
+            project = split[2].split('+');
+            slice = project[3].split(':');
+
+            store.push({'gn_address': gn, 'project': project[1], 'slice': slice[0]});
+          }
+        }
+      }
+      console.log(store);
+      res.json(store);
+    });
+  });
 
   app.get('/nodes', function(req, res) {
     
@@ -16,6 +53,7 @@ module.exports = function(app) {
       if (err)
         res.send(err);
 
+      console.log(nodes);
       res.json(nodes);
     });
   });
@@ -27,6 +65,7 @@ module.exports = function(app) {
       if (err)
         res.send(err);
 
+      console.log(services);
       res.json(services);
     });
   });
