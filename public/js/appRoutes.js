dc8ceebf21e7ebef2f57a03e1afbfa75151c4c89
Captain Underpants <mkeele@indiana.edu>
2014-05-18 23:07:28 -0400

    add more info to node and services

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -6,27 +6,24 @@
 
 angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
-  $routeProvider
-
-    .when('/', {
+  $routeProvider.
+    when('/', {
       templateUrl: 'views/home.html',
       controller: 'MainController'
-    })
-
-    .when('/nodes', {
+    }).
+    when('/nodes', {
       templateUrl: 'views/node.html',
       controller: 'NodeController'
-    })
-
-    .when('/services', {
+    }).
+    when('/services', {
       templateUrl: 'views/service.html',
       controller: 'ServiceController'
-    })
-
-    .when('/blipp', {
+    }).
+    when('/blipp', {
       templateUrl: 'views/blipp.html',
       controller: 'BlippController'
-    });
+    }).
+    otherwise({redirectTo: '/'});
 
   $locationProvider.html5Mode(true);
 
