7238f84872599820c5895fbd5ff01b9fc7bf9c8c
Captain Underpants <mkeele@indiana.edu>
2014-05-30 13:29:11 -0400

    add iperf test submission

--- public/js/controllers/BlippCtrl.js
+++ public/js/controllers/BlippCtrl.js
@@ -10,6 +10,7 @@ angular.module('BlippCtrl', []).controller('BlippController', function($scope, $
     .success(function(data) {
 
       $scope.pingData = {};
+      $scope.perfData = {};
       $scope.alerts = [];
       $scope.timeTypes = [
         {type:'Seconds'},
@@ -17,35 +18,13 @@ angular.module('BlippCtrl', []).controller('BlippController', function($scope, $
         {type:'Hours'},
         {type:'Days'}
       ];
-
-      /*var ping_measurement = {
-        "$schema": "http://unis.incntre.iu.edu/schema/20140214/measurement#",
-        "service": "http://localhost:8888/services/MXRRLAWJI94GMEDC",
-        "ts": 1398785926407953,
-        "eventTypes": [
-          "ps:tools:blipp:linux:net:ping:rtt",
-          "ps:tools:blipp:linux:net:ping:ttl"
-        ],
-        "configuration": {
-          "regex": "ttl=(?P<ttl>\\d+).*time=(?P<rtt>\\d+\\s|\\d+\\.\\d+)",
-          "reporting_params": 1,
-          "probe_module": "cmd_line_probe",
-          "schedule_params": {
-            "every": 5
-          },
-          "collection_schedule": "builtins.simple",
-          "command": "ping -c 1 156.56.5.10",
-          "collection_size": 10000000,
-          "ms_url": "http://localhost:8888",
-          "data_file": "/tmp/ops_ping.log",
-          "eventTypes": {
-            "rtt": "ps:tools:blipp:linux:net:ping:rtt",
-            "ttl": "ps:tools:blipp:linux:net:ping:ttl"
-          },
-          "collection_ttl": 1500000,
-          "name": "ping"
-        }
-      };*/
+      $scope.bandTesters = [
+        {type:'Iperf'}
+      ];
+      $scope.protos = [
+        {type:'TCP'},
+        {type:'UDP'}
+      ];
 
       $scope.addAlert = function(msg, type) {
         $scope.alerts.push({type: type, msg: msg});
@@ -123,6 +102,68 @@ angular.module('BlippCtrl', []).controller('BlippController', function($scope, $
           });
         }
       };
+      $scope.perfSubmit = function(perf) {
+
+        if (perf.$invalid) {
+          // If form is invalid, return and let AngularJS show validation errors.
+          $scope.addAlert('Invalid form, cannot be submitted', 'danger');
+          $scope.alert = true;
+          return;
+        } else {
+          // Tell user form has been sent
+          // $scope.addAlert('Data sent to UNIS. Please wait for confirmation.', 'info');
+          // $scope.alert = true;
+
+          // copy data submitted by form
+          $scope.perfData = angular.copy(perf);
+
+          var perf_measurement = {
+            "$schema": "http://unis.incntre.iu.edu/schema/20140214/measurement#",
+            "service": "http://localhost:8888/services/MXRRLAWJI94GMEDC",
+            "ts": 1398785926407953,
+            "eventTypes": [
+              "ps:tools:blipp:linux:net:iperf:bandwidth"
+            ],
+            "configuration": {
+              "regex": ",(?P<bandwidth>\\d+)$",
+              "probe_module": "cmd_line_probe",
+              "collection_schedule": "builtins.simple",
+              "command": "iperf " + $scope.perfData.th,
+              "ms_url": "http://localhost:8888",
+              "data_file": "/tmp/ops_iperf.log",
+              "eventTypes": {
+                "bandwidth": "ps:tools:blipp:linux:net:iperf:bandwidth"
+              },
+              "name": $scope.perfData.desc
+            }
+          };
+
+          $http({
+            method: 'POST',
+            url: '/api/measurements',
+            data: perf_measurement,
+            headers: {'Content-type': 'application/perfsonar+json'}
+          }).
+          success(function(data, status, headers, config) {
+            // $scope.addAlert(data, 'success');
+            // $scope.addAlert(status, 'success');
+            // $scope.addAlert(headers, 'success');
+            // $scope.addAlert(config, 'success');
+            var measurement = data;
+            $scope.addAlert('BLiPP Test: ' + measurement.configuration.name + ' submitted to UNIS', 'success');
+            $scope.alert = true;
+          }).
+          error(function(data, status, headers, config) {
+            // $scope.addAlert(data, 'danger');
+            // $scope.addAlert(status, 'danger');
+            // $scope.addAlert(headers, 'danger');
+            // $scope.addAlert(config, 'danger');
+            var measurement = data;
+            $scope.addAlert('Status: ' + status.toString() + ', ' + 'Error: ' + measurement.error.message, 'danger');
+            $scope.alert = true;
+          });
+        }
+      };
       $scope.pingReset = function() {
         // clear client and server side form
         // $scope.pingData = {};
@@ -138,9 +179,27 @@ angular.module('BlippCtrl', []).controller('BlippController', function($scope, $
         $scope.ping.reportMS = 10;
         $scope.closeAlert(0);
       };
+      $scope.perfReset = function() {
+        // clear client and server side form
+        // $scope.perfData = {};
+        // $scope.perf = angular.copy($scope.perfData);
+
+        // clear client side form and reset defaults
+        $scope.perf = angular.copy({});
+        $scope.perf.tbtValue = 4;
+        $scope.perf.tbtType = $scope.timeTypes[2];
+        $scope.perf.td = 20;
+        $scope.perf.bt = $scope.bandTesters[0];
+        $scope.perf.proto = $scope.protos[0];
+        $scope.closeAlert(0);
+      };
       $scope.pingReset();
       $scope.pingUnchanged = function(ping) {
         return angular.equals(ping, $scope.pingData);
+      };
+      $scope.perfReset();
+      $scope.perfUnchanged = function(perf) {
+        return angular.equals(perf, $scope.perfData);
       };
       $scope.togglePing = function() {
         $scope.addIperf = false;
@@ -166,6 +225,15 @@ angular.module('BlippCtrl', []).controller('BlippController', function($scope, $
         $scope.btnNetlogger = "btn btn-default";
         $scope.btnIperf = $scope.btnIperf === "btn btn-primary active" ? "btn btn-default": "btn btn-primary active";
         $scope.addIperf = $scope.addIperf === true ? false: true;
+
+        // default form values
+        $scope.perf = angular.copy({});
+        $scope.perf.tbtValue = 4;
+        $scope.perf.tbtType = $scope.timeTypes[2];
+        $scope.perf.td = 20;
+        $scope.perf.bt = $scope.bandTesters[0];
+        $scope.perf.proto = $scope.protos[0];
+        $scope.alert = false;
       };
       $scope.toggleNetlogger = function() {
         $scope.addPing = false;
@@ -176,8 +244,7 @@ angular.module('BlippCtrl', []).controller('BlippController', function($scope, $
         $scope.addNetlogger = $scope.addNetlogger === true ? false: true;
       };
       $scope.nodes = data;
-      $scope.filterNodes = function(node)
-      {
+      $scope.filterNodes = function(node) {
           if(node.name != 'GN0')
           {
               return true; // this will be listed in the results
