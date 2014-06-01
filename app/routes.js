bcccc83568b7bc2a07e78589144a51d523cdb6d3
Captain Underpants <mkeele@indiana.edu>
2014-06-01 15:35:09 -0400

    add more information from node.info file

--- app/routes.js
+++ app/routes.js
@@ -39,24 +39,35 @@ module.exports = function(app) {
         console.log(err);
         res.send(err);
       } else {
-        // console.log('received data: ' + data);
+        console.log('received data: ' + data);
 
         var fileData = data.toString().split('\n');
-        var split, project, slice, gn;
+        var split, project, slice, gn, exAddy, ms, uuid, unis;
 
         for(line in fileData) {
           split = fileData[line].split('=');
 
+          if(split[0] === 'external_address')
+            exAddy = split[1];
+
           if (split[0] === 'gn_address')
             gn = split[1];
+
+          if(split[0] === 'unis_instance')
+            unis = split[1];
+
+          if(split[0] === 'ms_instance')
+            ms = split[1];
 
           if (split[0] === 'node_id') {
             project = split[2].split('+');
             slice = project[3].split(':');
-
-            store.push({'gn_address': gn, 'project': project[1], 'slice': slice[0]});
           }
+
+          if(split[0] === 'auth_uuid')
+            uuid = split[1];
         }
+        store.push({'external_address': exAddy, 'gn_address': gn, 'unis_instance': unis, 'ms_url': ms, 'project': project[1], 'slice': slice[0], 'slice_uuid': uuid});
       }
       console.log(store);
       res.json(store);
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -8,10 +8,14 @@ angular.module('MainCtrl', []).controller('MainController', function($scope, $ht
 
   $http.get('/api/slice')
     .success(function(data) {
-      $scope.gn = data[0].gn_address;
-      $scope.slice = data[0].slice;
-      $scope.project = data[0].project;
       console.log(data);
+      $scope.exAddy = data[0].external_address;
+      $scope.gn = data[0].gn_address;
+      $scope.unis = data[0].unis_instance;
+      $scope.ms = data[0].ms_url;
+      $scope.project = data[0].project;
+      $scope.slice = data[0].slice;
+      $scope.uuid = data[0].slice_uuid;
     })
     .error(function(data) {
       console.log('Error: ' + data);
