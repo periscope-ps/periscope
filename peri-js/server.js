f5311d4e638172006177663bdbb2e079521a3fce
Mark Keele <mkeele@indiana.edu>
2014-06-08 22:12:47 -0400

    raise server port number

--- peri-js/server.js
+++ peri-js/server.js
@@ -26,7 +26,7 @@ var app = express();
 
 /* App Configuration */
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 42424);
   app.use(express.static(__dirname + '/public'));
   app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.use(express.logger('dev'));
