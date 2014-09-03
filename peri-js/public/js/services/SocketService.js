898241e1763657d7b5147ac1563c4c1491dfdb0a
Mark Keele <mkeele@indiana.edu>
2014-09-03 00:55:55 -0400

    generic socket service, connects to nodejs server, on and emit events

---
+++ peri-js/public/js/services/SocketService.js
@@ -0,0 +1,30 @@
+/*
+ * Socket Service
+ * public/js/services/
+ * SocketService.js
+ */
+
+angular.module('SocketService', []).service('Socket', function($rootScope) {
+
+  var socket = io.connect('http://localhost:42424');
+
+  this.on = function (eventName, callback) {
+    socket.on(eventName, function () {
+      var args = arguments;
+      $rootScope.$apply(function () {
+        callback.apply(socket, args);
+      });
+    });
+  };
+  this.emit = function (eventName, data, callback) {
+    socket.emit(eventName, data, function () {
+      var args = arguments;
+      $rootScope.$apply(function () {
+        if (callback) {
+          callback.apply(socket, args);
+        }
+      });
+    })
+  };
+
+});
