e2fccaf8f493586daaa1cb3ed937ed9321fc989e
Mark Keele <mkeele@indiana.edu>
2014-09-03 00:52:22 -0400

    client side socket for node

--- peri-js/public/js/controllers/NodeCtrl.js
+++ peri-js/public/js/controllers/NodeCtrl.js
@@ -4,10 +4,20 @@
  * NodeCtrl.js
  */
 
-angular.module('NodeCtrl', []).controller('NodeController', function($scope, Node) {
+angular.module('NodeCtrl', []).controller('NodeController', function($scope, Node, Socket) {
 
   Node.getNodes(function(nodes) {
     $scope.nodes = nodes;
   });
+
+  Socket.emit('cs_emit', 'hello node');
+
+  Socket.on('ss_emit', function (data) {
+    $scope.hello_socket = data;
+  });
+
+  /*Socket.on('node_data', function (data) {
+    $scope.node_data = data;
+  });*/
 
 });
