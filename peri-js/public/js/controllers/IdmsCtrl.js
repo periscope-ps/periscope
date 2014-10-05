71edbb07f89bd4044a6f619f89c642e415c099aa
Mark Keele <mkeele@indiana.edu>
2014-10-05 19:56:48 -0400

    services and functions for idms page

--- peri-js/public/js/controllers/IdmsCtrl.js
+++ peri-js/public/js/controllers/IdmsCtrl.js
@@ -6,6 +6,78 @@
 
 angular.module('IdmsCtrl', []).controller('IdmsController', function($scope, $routeParams, $location, Idms) {
 
+  Idms.getNodes(function(nodes) {
+    $scope.nodes = $scope.nodes || [];
 
+    if (typeof nodes =='string')
+      nodes = JSON.parse(nodes);
+
+    $scope.nodes = $scope.nodes.concat(nodes);
+  });
+
+  Idms.getServices(function(services) {
+    $scope.services = $scope.services || [];
+
+    if (typeof services =='string')
+      services = JSON.parse(services);
+
+    $scope.services = $scope.services.concat(services);
+  });
+
+  Idms.getMeasurements(function(measurements) {
+    $scope.measurements = $scope.measurements || [];
+
+    if (typeof measurements =='string')
+      measurements = JSON.parse(measurements);
+
+    $scope.measurements = $scope.measurements.concat(measurements);
+  });
+
+  Idms.getMetadatas(function(metadata) {
+    $scope.metadata = $scope.metadata || [];
+
+    if (typeof metadata =='string')
+      metadata = JSON.parse(metadata);
+
+    $scope.metadata = $scope.metadata.concat(metadata);
+  });
+
+  $scope.getServiceNode = function(accessPoint) {
+    var ip = accessPoint.split(':')[1].replace('//', '');
+
+    for(var i = 0; i < $scope.nodes.length; i++) {
+      if ($scope.nodes[i].properties.geni.logins[0].hostname == ip) {
+          return $scope.nodes[i].id;
+      }
+    }
+  };
+  $scope.getServiceMeasurement = function(accessPoint) {
+    var ip = accessPoint.split(':')[1].replace('//', '');
+
+    for(var i = 0; i < $scope.measurements.length; i++) {
+      if($scope.measurements[i].configuration.command) {
+        if($scope.measurements[i].configuration.command.split(" ")[1] == ip) {
+          return $scope.measurements[i].eventTypes;
+        }
+      }
+    }
+  };
+  $scope.getServiceMetadata = function(accessPoint) {
+    var ip = accessPoint.split(':')[1].replace('//', '');
+    var metadatas = [];
+
+    for(var i = 0; i < $scope.measurements.length; i++) {
+      if($scope.measurements[i].configuration.command) {
+        if($scope.measurements[i].configuration.command.split(" ")[1] == ip) {
+          for(var j = 0; j < $scope.metadata.length; j++) {
+            if($scope.metadata[j].parameters.measurement.href.split('/')[4] == $scope.measurements[i].id) {
+              metadatas.push($scope.metadata[j].id);
+            }
+          }
+        }
+      }
+    }
+    return metadatas;
+  };
 
 });
--- peri-js/public/js/services/IdmsService.js
+++ peri-js/public/js/services/IdmsService.js
@@ -6,4 +6,73 @@
 
 angular.module('IdmsService', []).service('Idms', function($http, $routeParams) {
 
+  this.getNodes = function(nodes) {
+    $http.get('/unis/idms/nodes').success(function(data) {
+      console.log('HTTP Node Request: ' , data);
+      nodes(data);
+    }).error(function(data) {
+      console.log('HTTP Node Error: ' , data);
+    });
+  };
+
+  this.getServices = function(services) {
+    $http.get('/unis/idms/services').success(function(data) {
+      console.log('Service Request: ' , data);
+      services(data);
+    }).error(function(data) {
+      console.log('HTTP Service Error: ' ,  data);
+    });
+  };
+
+  this.getMeasurements = function(measurements) {
+    $http.get('/unis/idms/measurements/').success(function(data) {
+      console.log('Measurement Request: ' + data);
+      measurements(data);
+    }).error(function(data) {
+      console.log('Measurement Error: ' + data);
+    });
+  };
+
+  this.getMeasurement = function(measurement) {
+    $http.get('/unis/idms/measurements/' + $routeParams.id)
+      .success(function(data) {
+        console.log('Measurement Request: ' + data);
+        measurement(data);
+      })
+      .error(function(data) {
+        console.log('Measurement Error: ' + data);
+      });
+  };
+
+  this.getMetadatas = function(metadata) {
+    $http.get('/unis/idms/metadata').success(function(data) {
+      console.log('Metadata Request: ' + data);
+      metadata(data);
+    }).error(function(data) {
+      console.log('Metadata Error: ' + data);
+    });
+  };
+
+  this.getMetadata = function(metadata) {
+    $http.get('/unis/idms/metadata/' + $routeParams.id)
+      .success(function(data) {
+        console.log('Metadata Request: ' + data);
+        metadata(data);
+      })
+      .error(function(data) {
+        console.log('Metadata Error: ' + data);
+      });
+  };
+
+  this.getMetadataData = function(metadataData) {
+    var data_id = $routeParams.id;
+
+    $http.get('/unis/idms/data/' + data_id).success(function(data) {
+      console.log('Data Request: ' + data);
+      metadataData(data);
+    }).error(function(data) {
+      console.log('Data Error: ' + data);
+    });
+  };
+
 });
