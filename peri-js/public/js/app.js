9e8b251288b5b533ceeb6bcc16f0e2c41e20d095
Mark Keele <mkeele@indiana.edu>
2014-10-20 15:53:01 -0400

    add loading bar

--- peri-js/public/js/app.js
+++ peri-js/public/js/app.js
@@ -4,4 +4,4 @@
  * app.js
  */
 
-angular.module('measurementApp', ['ngRoute', 'ngAnimate', 'ui.utils' ,'ui.bootstrap', 'nvd3ChartDirectives', 'directedGraphModule', 'appRoutes', 'SliceCtrl', 'SliceService', 'NodeCtrl', 'NodeService', 'ServiceCtrl', 'ServiceService', 'BlippCtrl', 'BlippService', 'MeasurementCtrl', 'MeasurementService', 'HelmCtrl', 'HelmService', 'HelpCtrl', 'HelpService', 'MetadataCtrl', 'MetadataService', 'PortService', 'SocketService', 'schemaForm']);
+angular.module('measurementApp', ['ngRoute', 'ngAnimate', 'ui.utils' ,'ui.bootstrap', 'angular-loading-bar', 'nvd3ChartDirectives', 'directedGraphModule', 'appRoutes', 'SliceCtrl', 'SliceService', 'NodeCtrl', 'NodeService', 'ServiceCtrl', 'ServiceService', 'BlippCtrl', 'BlippService', 'MeasurementCtrl', 'MeasurementService', 'HelmCtrl', 'HelmService', 'HelpCtrl', 'HelpService', 'MetadataCtrl', 'MetadataService', 'PortService', 'SocketService', 'schemaForm']);
